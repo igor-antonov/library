72aa29a4483ffeaffec1d3a9c02c11e047adf399
Igor Antonov <antonov-i.v@yandex.ru>
2019-05-18 19:46:13 +0300

    Обернуть внешние вызовы в Hystrix

--- src/main/java/ru/otus/library/LibraryApplication.java
+++ src/main/java/ru/otus/library/LibraryApplication.java
@@ -2,11 +2,13 @@ package ru.otus.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class LibraryApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
--- src/main/java/ru/otus/library/service/BookService.java
+++ src/main/java/ru/otus/library/service/BookService.java
@@ -1,5 +1,8 @@
 package ru.otus.library.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.library.exception.DataNotFoundException;
@@ -14,19 +17,23 @@ import java.util.Optional;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final Logger log = LoggerFactory.getLogger(BookService.class);
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackBook", groupKey = "BookService", commandKey = "add")
     public Book add(Book book) {
         return bookRepository.insert(book);
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackAll", groupKey = "BookService", commandKey = "getAll")
     public List<Book> getAll() {
         return bookRepository.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackBook", groupKey = "BookService", commandKey = "getById")
     public Book getById(String id) throws DataNotFoundException {
         Optional<Book> bookOpt = bookRepository.findById(id);
         if (!bookOpt.isPresent()){
@@ -37,14 +44,46 @@ public class BookService {
         }
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackId", groupKey = "BookService", commandKey = "updateById")
     public void updateById(String bookId, Book book) throws DataNotFoundException {
         Book bookOld = getById(bookId);
         book.setId(bookOld.getId());
         bookRepository.save(book);
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackId", groupKey = "BookService", commandKey = "deleteById")
     public void deleteById(String id) throws DataNotFoundException {
         getById(id);
         bookRepository.deleteById(id);
+    }
+
+    public void fallbackId(String bookId, Book book) throws DataNotFoundException {
+        String errMessage = "Что-то пошло не так. Обратитесь к администратору";
+        log.error(errMessage);
+        throw new DataNotFoundException(errMessage);
+    }
+
+    public void fallbackId(String id) throws DataNotFoundException {
+        String errMessage = "Что-то пошло не так. Обратитесь к администратору";
+        log.error(errMessage);
+        throw new DataNotFoundException(errMessage);
+    }
+
+    public Book fallbackBook(String id) throws DataNotFoundException {
+        String errMessage = "Что-то пошло не так. Обратитесь к администратору";
+        log.error(errMessage);
+        throw new DataNotFoundException(errMessage);
+    }
+
+    public Book fallbackBook(Book book) throws DataNotFoundException {
+        String errMessage = "Что-то пошло не так. Обратитесь к администратору";
+        log.error(errMessage);
+        throw new DataNotFoundException(errMessage);
+    }
+
+    public List<Book> fallbackAll() throws DataNotFoundException {
+        String errMessage = "Что-то пошло не так. Обратитесь к администратору";
+        log.error(errMessage);
+        throw new DataNotFoundException(errMessage);
     }
 }
